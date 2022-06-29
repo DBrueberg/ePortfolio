ba5e8e855a7198d7cde9c0bb68414779dd2ee908
Devin Brueberg <brueberd@csp.edu>
2022-06-29 09:39:00 -0500

    Initial project layout in progress. Need to finalize layout

--- front-end/src/component/main/Project.js
+++ front-end/src/component/main/Project.js
@@ -7,16 +7,32 @@
 // Using React library in order to build components 
 // for the app and importing needed components
 import React from 'react';
-import { MDBTypography } from 'mdb-react-ui-kit';
+import { MDBContainer, MDBIcon, MDBTypography } from 'mdb-react-ui-kit';
 
 
 function Project() {
   return (
-    <div className="Project">
-      <MDBTypography variant='h1'>
-        Welcome to the Project Page!
+    <MDBContainer className="Project">
+      <MDBTypography className="text-center" variant='h1'>
+        Project
       </MDBTypography>
-    </div>
+      <MDBTypography className="mx-auto" listUnStyled style={{ maxWidth: "42rem" }}>
+        <li className="mb-2">
+          <MDBTypography className="mb-1" style={{ fontSize: "large", fontWeight: "bold" }} >
+            Restaurant Club
+          </MDBTypography>
+          <MDBTypography  listUnStyled >
+            <li style={{ fontSize: "small" }}>
+              A fully responsive SPA React web application that allows users to create and share restaurant reviews
+            </li>
+            <li>
+            <MDBIcon className="me-1" fas icon="link" />
+            <MDBIcon className="me-1" fab icon="github" />
+            </li>
+          </MDBTypography>
+        </li>
+      </MDBTypography>
+    </MDBContainer>
   );
 }
 
