5c473c5c20445083a041b80db9f77eda5a256465
Devin Brueberg <brueberd@gmail.com>
2022-06-30 16:53:20 -0500

    Main content and layout complete

--- front-end/src/component/main/Main.js
+++ front-end/src/component/main/Main.js
@@ -3,26 +3,107 @@
 // E-Portfolio - App.js
 // June 9, 2022
 // Last Edited (Initials, Date, Edits):
+//  (DAB, 6/30/2022, Created initial Main page and content)
 
-// Using React library in order to build components 
+// Using React library in order to build components
 // for the app and importing needed components
-import React from 'react';
-import { MDBRipple } from 'mdb-react-ui-kit';
+import React from "react";
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardHeader,
+    MDBCardImage,
+    MDBCol,
+    MDBContainer,
+    MDBRipple,
+    MDBRow,
+    MDBTypography,
+} from "mdb-react-ui-kit";
 
 function Main() {
-  return (
-    <div className="Main">
-      <p>Hello World....</p>
-      <MDBRipple rippleTag='div' className='bg-image'>
-        <img src='https://mdbootstrap.com/img/new/standard/city/053.webp' className='w-100' />
-        <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.6)' }}></div>
-      </MDBRipple>
-      <MDBRipple rippleTag='div' rippleColor='light' className='bg-image'>
-        <img src='https://mdbootstrap.com/img/new/standard/city/053.webp' className='w-100' />
-        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}></div>
-      </MDBRipple>
-    </div>
-  );
+    return (
+        <MDBContainer className="Main">
+            <MDBCard>
+                <MDBRow className="pt-4 g-0">
+                    <MDBCol className="m-auto" sm="6">
+                        <MDBCard className="x-auto h-100">
+                            <MDBRipple
+                                className="bg-image hover-overlay"
+                                rippleTag="div"
+                            >
+                                <MDBCardImage
+                                    className="w-100"
+                                    src="me.jpg"
+                                    alt="Devin"
+                                    fluid
+                                    style={{ borderRadius: ".25rem" }}
+                                />
+                                <a>
+                                    <div
+                                        className="mask"
+                                        style={{
+                                            backgroundColor:
+                                                "rgba(0, 0, 0, 0.1)",
+                                        }}
+                                    ></div>
+                                </a>
+                            </MDBRipple>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol sm="6">
+                        <MDBCard className="mx-auto h-100">
+                            <MDBCardBody>
+                                <MDBCardHeader className="ps-0" tag="h1">
+                                    Hello
+                                </MDBCardHeader>
+                                <MDBTypography>
+                                    My name is Devin Brueberg and have held an
+                                    interest in Software Development for many
+                                    years now. My current focus is on full stack web
+                                    development using mainly React but I am
+                                    open to other projects. I have a wide array
+                                    of development skills and am interested in
+                                    anything related to development.
+                                </MDBTypography>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+            </MDBCard>
+
+            {/* <MDBRow className="pt-4">
+                <MDBCol>
+                    <MDBCard className="mx-auto h-100">
+                        <MDBRipple
+                            rippleTag="div"
+                            className="bg-image hover-overlay"
+                        >
+                            <img src="me.jpg" className="w-100" />
+                            <div
+                                className="mask"
+                                style={{
+                                    backgroundColor: "rgba(0, 0, 0, 0.1)",
+                                }}
+                            ></div>
+                        </MDBRipple>
+                    </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                    <MDBCard className="mx-auto h-100">
+                        <MDBCardBody>
+                            <MDBCardHeader className="ps-0" tag="h1">
+                                Hello
+                            </MDBCardHeader>
+                            <MDBTypography>
+                                I am Devin Brueberg and have held an interest in
+                                Software Development
+                            </MDBTypography>
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+            </MDBRow> */}
+        </MDBContainer>
+    );
 }
 
 export default Main;
